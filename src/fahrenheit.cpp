bdcaab402953f50934ce6a8fe87f817b54d8f937
IsaqueZaratustra <ADSisaque@gmail.com>
2022-08-15 16:13:13 -0300

    add rev

--- src/fahrenheit.cpp
+++ src/fahrenheit.cpp
@@ -4,16 +4,16 @@ int main(int argc, char const *argv[])
 {
     int num, recebe;
 
-    std::cout << "Digite o numero que voce deseja converter, em Celsius: " << std::endl;
+    std::cout << "Digite o numero que voce deseja converter, em Fahrenheit: " << std::endl;
     std::cin >> num;
-    std::cout << "Deseja converter esse numero para: \n 1) Kelvin \n 2) Fahrenheit" << std::endl;
+    std::cout << "Deseja converter esse numero para: \n 1) Celsius \n 2) Kelvin" << std::endl;
     std:: cin >> recebe;
     switch (recebe)
     {
-    case 1: std::cout << "Convertido de Celsius para kelvin: " << num + 273 << std::endl;      
+    case 1: std::cout << "Convertido de Fahrenheit para Celsius: " << (num - 32) / 1.8 << std::endl;      
         break;
 
-    case 2: std::cout << "Convertido de Celsius para Fahrenheit: " << num * 1.8 + 32 << std::endl;      
+    case 2: std::cout << "Convertido de Fahrenheit para Kelvin: " << (num - 32) * 5 / 9 + 273 << std::endl;      
         break;
     
     default:
